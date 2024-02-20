879bd08777000c4630ca8e781921dcbc12694be9
dmuras <dawid.muras04@gmail.com>
2024-02-20 19:24:03 +0100

    navbar & cart page

--- src/pages/components/navbar.jsx
+++ src/pages/components/navbar.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 function Navbar() {
   return (
-    <div>
-      
+    <div className='navbar-container'>
+        <div className="navbar-lt">
+                <h4 style={{color: "white"}}><em>fastest shipping...</em></h4>
+        <div className="navbar-logo">
+                <Image src="/shoppingCart.png"  height={50} width={50}/>
+        </div>
+        </div>
+        <div className="navbar-menu">
+            <Link className="cart" href="/cart"><Image src="/cart.png" height={50} width={50}/> </Link>
+        </div>
     </div>
   )
 }
